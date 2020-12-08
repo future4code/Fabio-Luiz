fa255c4e928284ea718309afac53ed4fa4978b25
fsantos22 <22fsantos@gmail.com>
2020-12-08 16:49:09 -0300

    Aula 14 - DOM

---
+++ semana4/aula14/exercicio/index.js
@@ -0,0 +1,31 @@
+function criarTarefas() {
+    const task = document.getElementById('tarefa')
+    const dia = document.getElementById('dias-semana')
+    const horario = document.getElementById('horario')
+    if (task.value) {
+        document.getElementById(dia.value).innerHTML += `<p class="item-lista">${horario.value} - ${task.value}</p>`
+    }
+    else {
+        alert("Insira uma tarefa antes!")
+    }
+    task.value = ""
+}
+const btnCriar = document.getElementById("btn-criar")
+btnCriar.addEventListener('click', criarTarefas)
+
+
+function limparLista() {
+    const diaPlanner = document.getElementsByClassName("dias-planner")
+    for (let i=0; i < diaPlanner.length; i++) {
+        diaPlanner[i].innerHTML = ''
+    }
+}
+const btnLimpar = document.getElementById("btn-limpar")
+btnLimpar.addEventListener('click', limparLista)
+
+function func(e) {
+    let x = event.target
+    if (x.tagName === "P") {
+        x.classList.toggle("riscado")
+    }
+}
