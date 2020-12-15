fbd14bb3d91c8b2c2a838f40e2409f3bff45ac01
fsantos22 <22fsantos@gmail.com>
2020-12-15 00:43:16 -0300

    Aula 17 - Introdução ao React

--- semana5/aula17/labtube-react/src/App.js
+++ semana5/aula17/labtube-react/src/App.js
@@ -3,10 +3,12 @@ import trendIcon from './img/trend_icon.png'
 import subscribeIcon from './img/subscribes_icon.png'
 import libraryIcon from './img/library_icon.png'
 import historyIcon from './img/history_icon.png'
+import labTubeLogo from './img/labtube_logo.png'
+import searchLogo from './img/search_icon.png'
 import './App.css';
 
 function App() {
-  const titulo = "Titulo do video"
+  const titulo = "Título do video"
 
   function reproduzVideo() {
     alert("O vídeo está sendo reproduzido")
@@ -16,66 +18,105 @@ function App() {
     <div>
       <div className="tela-inteira">
         <header>
-            <h1>Lab Tube</h1>
-            <input type="text" placeholder="Busca" id="campoDeBusca" />
+            <img src={labTubeLogo} alt="LabTube logo"/>
+            <div className="search-box">
+              <input type="text" placeholder="Busca" id="campoDeBusca" />
+              <img src={searchLogo} />
+            </div>
         </header>
 
         <main>
             <nav className="menu-vertical">
                 <ul>
                     <li className="botoes-meunu-vertical botao-inicio">
-                      <img src={homeIcon}/><span>Início</span></li>
+                      <img src={homeIcon} alt="Início" /><span>Início</span></li>
                     <li className="botoes-meunu-vertical botao-trend">
-                      <img src={trendIcon}/><span>Em alta</span></li>
+                      <img src={trendIcon} alt="Em Alta" /><span>Em alta</span></li>
                     <li className="botoes-meunu-vertical">
-                      <img src={subscribeIcon}/><span>Inscrições</span></li>
+                      <img src={subscribeIcon} alt="Inscrições" /><span>Inscrições</span></li>
                     <hr />
                     <li className="botoes-meunu-vertical">
-                      <img src={libraryIcon}/><span>Originais</span></li>
+                      <img src={libraryIcon} alt="Originais" /><span>Originais</span></li>
                     <li className="botoes-meunu-vertical">
-                      <img src={historyIcon}/><span>Histórico</span></li>
+                      <img src={historyIcon} alt="Histórico" /><span>Histórico</span></li>
                 </ul>
             </nav>
             
             <section className="painel-de-videos">
                 <div className="box-pagina-principal media1" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=1 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media2" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=2 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy1" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media3" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=3 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy2" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media4" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=4 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy3" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media5" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=5 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy4" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media6" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=6 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy5" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media7" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=7 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy6" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
                 <div className="box-pagina-principal media8" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=8 " alt="" />
-                    <h4>{titulo}</h4>
+                    <div>
+                      <div className="videoUser">
+                        <img src="https://source.unsplash.com/random/?guy" alt="" />
+                      </div>
+                      <h4>{titulo}</h4>
+                    </div>
                 </div>
             </section>
         </main>
-
-        <footer>
-            <h4>Desenvolvido por <span>Fabio Dev</span></h4>
-        </footer>
       </div>
     </div>
   );
