7b654ce79a5f6cfee17b28f7219dffe3cb611a6c
fsantos22 <22fsantos@gmail.com>
2020-12-30 23:29:40 -0300

    Aula 20 - Listas e Estilização

---
+++ semana5/aula20/insta4/src/PostsData.js
@@ -0,0 +1,19 @@
+const postData = [
+    {
+        nomeUsuario: 'Biroliro',
+        fotoUsuario: 'https://static.poder360.com.br/2020/12/bolsonaro-risada-868x644.jpg',
+        fotoPost: 'https://picsum.photos/200/150?a',
+    },
+    {
+        nomeUsuario: 'Bilula',
+        fotoUsuario: 'https://veja.abril.com.br/wp-content/uploads/2017/10/brasil-ex-presidente-lula-20171009-002.jpg',
+        fotoPost: 'https://picsum.photos/200/150?b',
+    },
+    {
+        nomeUsuario: 'Otário de Carvalho',
+        fotoUsuario: 'https://www.rbsdirect.com.br/imagesrc/24819237.jpg',
+        fotoPost: 'https://picsum.photos/200/150?c',
+    }
+]
+
+export default postData
